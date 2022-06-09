667166fd04d3672712d745fddf8624a25fa0a075
Roman Skrypnyk <skripnik.rma@ukr.net>
2022-06-09 21:36:25 +0300

    Create oopInJs.js

---
+++ oopInJs.js
@@ -0,0 +1,27 @@
+const Animal = function(sound){
+    this.sound = sound;
+  };
+  
+  Animal.prototype.makeSound = function(){
+    console.log(this.sound);
+  };
+  
+  const Dog = function(name, breed){
+    this.name = name;
+    this.breed = breed;
+  }
+  
+  Dog.prototype = new Animal('gaf-gat');
+  
+  const Cat = function(name, breed){
+    this.name = name;
+    this.breed = breed;
+  };
+  
+  Cat.prototype = new Animal('mieo-mieo');
+  
+  const dog = new Dog('Dog', 'Doggiz');
+  const cat = new Cat('Cat', 'Kitty');
+  
+  dog.makeSound();
+  cat.makeSound();
